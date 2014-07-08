91fce3f4a024a6d71abe27d0d011019b43ea0218
Andrey Sumin <an.sumin@hh.ru>
2014-07-08 15:21:43 +0400

    HH-44603 add new tests

--- pages/__init__.py
+++ pages/__init__.py
@@ -6,5 +6,5 @@ import frontik.handler
 class Page(frontik.handler.PageHandler):
     def get_page(self):
         self.json.put({
-            'Hello': 'world'
+            'Hello': self.get_argument('param')
         })
